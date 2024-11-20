1c9ea648a95da09a38b025e3fea28cb0d4c4d410
Saúl Maldonado lópez <Alejandrom199916@gmail.com>
2024-11-20 18:43:10 -0500

    cambios 3

--- proyecto-dawa/src/app/models/Empresa.ts
+++
@@ -1,9 +0,0 @@
-export interface Empresa{
-    id?: number
-    nombre_empresa: string
-    direccion: string
-    telefono: string
-    email: string
-    hora_recoleccion: string
-    material: string
-}
--- proyecto-dawa/src/app/models/Material.ts
+++
@@ -1,8 +0,0 @@
-export interface Material{
-    id?: number
-    //id_hogar?: number
-    nombre: string;
-    cantidad: number
-    unidad: string;
-    fecha_registro: string
-}
